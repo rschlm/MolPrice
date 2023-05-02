9dfa289605d4f12a174aaa700b6ab0400ebcab64
loicbassement <loic.bas83@orange.fr>
2023-05-02 18:26:36 +0200

    set-up
    just some set-up

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from torchvision import transforms, datasets
+import sklearn
+import pandas as pd
+import rdkit
+import random
+
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+np.random.seed(0)
+random.seed(0)
+
+
+
+
 
