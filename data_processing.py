c98b14d7676b5f8aad83f88707119f6942849c9a
loicbassement <loic.bas83@orange.fr>
2023-05-03 16:11:13 +0200

    regarder les data

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-from torchvision import transforms, datasets
-import sklearn
+#from torchvision import transforms, datasets
+#import sklearn
 import pandas as pd
 import rdkit
 import random
@@ -12,7 +12,5 @@ torch.cuda.manual_seed(0)
 np.random.seed(0)
 random.seed(0)
 
-
-
-
-
+data=pd.read_csv("mcule_purchasable_in_stock_prices_230324_RKoqmy.csv", delimiter=",", low_memory=False)
+print(data.head())
